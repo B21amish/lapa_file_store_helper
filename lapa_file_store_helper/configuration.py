80d9c104116c18bd34ac0cc28bc4832383347670
Lav Sharma <lavsharma2016@gmail.com>
2024-02-02 22:28:10 +0530

    lapa_commons is used for reading configuration

--- lapa_file_store_helper/configuration.py
+++ lapa_file_store_helper/configuration.py
@@ -1,16 +1,11 @@
-import configparser
 import os
 
-config = configparser.ConfigParser()
-config_file_path = (
-    os.path.dirname(os.path.abspath(__file__)) + os.sep + "data" + os.sep + "config.ini"
-)
-config.read(config_file_path)
+from lapa_commons.main import read_configuration_from_file_path
+
+config_file_path = os.path.dirname(os.path.abspath(__file__)) + os.sep + "data" + os.sep + "config.ini"
+ldict_configuration = read_configuration_from_file_path(config_file_path)
 
 # get all vars and typecast
-
-config_str_lapa_file_store_protocol = config.get(
-    "ENVIRONMENT", "LAPA_FILE_STORE_PROTOCOL"
-)
-config_str_lapa_file_store_ip = config.get("ENVIRONMENT", "LAPA_FILE_STORE_IP")
-config_int_lapa_file_store_port = int(config.get("ENVIRONMENT", "LAPA_FILE_STORE_PORT"))
+config_str_lapa_file_store_protocol = str(ldict_configuration["ENVIRONMENT"]["LAPA_FILE_STORE_PROTOCOL"])
+config_str_lapa_file_store_ip = str(ldict_configuration["ENVIRONMENT"]["LAPA_FILE_STORE_IP"])
+config_int_lapa_file_store_port = int(ldict_configuration["ENVIRONMENT"]["LAPA_FILE_STORE_PORT"])
